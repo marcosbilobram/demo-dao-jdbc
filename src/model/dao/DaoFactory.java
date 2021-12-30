7067869f27aa5d5a777c2345c0e2721f7be3cae0
Marcos Henrique <marcosbilobram@hotmail.com>
2021-12-30 16:47:37 -0300

    DepartmentDao implementation and test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
@@ -8,5 +9,7 @@ public class DaoFactory {
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
 	}
-
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
+	}
 }
